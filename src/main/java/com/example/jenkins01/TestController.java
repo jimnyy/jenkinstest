839ab9fead092c6d6d7777129b305a62a1d8f4dc
jimnyy <dojimny1004@gmail.com>
2024-08-23 14:51:12 +0900

    test2

--- src/main/java/com/example/jenkins01/TestController.java
+++ src/main/java/com/example/jenkins01/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("test");
+        return ResponseEntity.ok("tests");
     }
 }
